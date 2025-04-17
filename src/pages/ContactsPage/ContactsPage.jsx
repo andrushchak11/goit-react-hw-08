21899477beffb9ef5782b3791d58a9e18f63714e
Liza Andrushchak <andrushchakliza11@gmail.com>
2025-04-17 21:04:20 +0300

    add query for all contacts

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import ContactForm from "../../components/ContactForm/ContactForm";
 import ContactList from "../../components/ContactList/ContactList";
 import SearchBox from "../../components/SearchBox/SearchBox";
+import { fetchContact } from "../../redux/contacts/operations";
+import { selectIsLoggedIn } from "../../redux/auth/selectors";
 import css from "./ContactsPage.module.css";
 
 function ContactsPage() {
+  const dispatch = useDispatch();
+  const isLoggedIn = useSelector(selectIsLoggedIn);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      dispatch(fetchContact());
+    }
+  }, [dispatch, isLoggedIn]);
+
   return (
     <div className={css.container}>
       <h2>Мої контакти</h2>
